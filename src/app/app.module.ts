284d7a257068db71aa5c36b6bab82610b4fc14c0
pilhhhha <54607401+pilhhhha@users.noreply.github.com>
2021-09-21 21:55:17 -0300

    Iniciativa do Projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cadastro',
+  templateUrl: './cadastro.component.html' 
+})
+export class CadastroComponent {
+}
