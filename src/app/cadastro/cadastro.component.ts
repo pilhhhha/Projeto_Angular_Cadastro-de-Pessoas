aae09e68645fe588dab7a0d67139c012fe83f310
pilhhhha <54607401+pilhhhha@users.noreply.github.com>
2021-09-22 22:34:23 -0300

    Finalizado

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -5,4 +5,93 @@ import { Component } from '@angular/core';
   templateUrl: './cadastro.component.html' 
 })
 export class CadastroComponent {
+title = 'exercicio-formulario';
+
+  esconderBotaoNovo = false;
+  esconderBotaoAlterar = false;
+  esconderBotaoExcluir = false;
+  esconderBotaoProcurar = false;
+  esconderBotaoGravar = true;
+  esconderBotaoCancelar = true;
+  esconderBotaoConfirmarExclusao = true;
+  esconderCampoProcura = true;
+  esconderBotaoProcurarAcao = true;
+
+  desabilitarCamposCadastro = true;
+
+  novo(){
+    this.esconderBotaoNovo = true;
+    this.esconderBotaoAlterar = true;
+    this.esconderBotaoExcluir = true;
+    this.esconderBotaoProcurar = true;
+    this.esconderBotaoGravar = false;
+    this.esconderBotaoCancelar = false;
+    this.desabilitarCamposCadastro = false;
+  }
+
+  alterar(){
+    this.esconderBotaoNovo = true;
+    this.esconderBotaoAlterar = true;
+    this.esconderBotaoExcluir = true;
+    this.esconderBotaoProcurar = true;
+    this.esconderBotaoGravar = false;
+    this.esconderBotaoCancelar = false;
+    this.desabilitarCamposCadastro = false;
+  }
+
+  excluir(){
+    this.esconderBotaoNovo = true;
+    this.esconderBotaoAlterar = true;
+    this.esconderBotaoExcluir = true;
+    this.esconderBotaoProcurar = true;
+    this.esconderBotaoGravar = true;
+    this.esconderBotaoConfirmarExclusao = false;
+    this.esconderBotaoCancelar = false;
+  }
+
+  gravar(){
+    this.esconderBotaoNovo = false;
+    this.esconderBotaoAlterar = false;
+    this.esconderBotaoExcluir = false;
+    this.esconderBotaoProcurar = false;
+    this.esconderBotaoGravar = true;
+    this.esconderBotaoCancelar = true;
+    this.esconderBotaoConfirmarExclusao = true;
+    this.desabilitarCamposCadastro = true;
+  }
+
+  procurarOpcao(){
+    this.esconderBotaoNovo = true;
+    this.esconderBotaoAlterar = true;
+    this.esconderBotaoExcluir = true;
+    this.esconderBotaoProcurar = true;
+    this.esconderBotaoProcurarAcao = false;
+    this.esconderBotaoGravar = true;
+    this.esconderBotaoConfirmarExclusao = true;
+    this.esconderBotaoCancelar = true;
+    this.esconderCampoProcura = false;
+  }
+
+  procurarAcao(){
+    this.esconderBotaoNovo = false;
+    this.esconderBotaoAlterar = false;
+    this.esconderBotaoExcluir = false;
+    this.esconderBotaoProcurar = false;
+    this.esconderBotaoGravar = true;
+    this.esconderBotaoCancelar = true;
+    this.esconderBotaoConfirmarExclusao = true;
+    this.esconderCampoProcura = true;
+    this.esconderBotaoProcurarAcao = true;
+  }
+
+  cancelar(){
+    this.esconderBotaoNovo = false;
+    this.esconderBotaoAlterar = false;
+    this.esconderBotaoExcluir = false;
+    this.esconderBotaoProcurar = false;
+    this.esconderBotaoGravar = true;
+    this.esconderBotaoCancelar = true;
+    this.esconderBotaoConfirmarExclusao = true;
+    this.desabilitarCamposCadastro = true;
+  }
 }
